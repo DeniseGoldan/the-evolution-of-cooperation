8422b70a4e2dd8d7b8119dfdab6d4760797d0d23
denise <denise.goldan@gmail.com>
2018-04-18 13:35:02 +0300

    Changed line charts library

--- TheEvolutionOfCooperation/src/charts/TournamentEvolutionLineChart.java
+++ TheEvolutionOfCooperation/src/charts/TournamentEvolutionLineChart.java
@@ -1,55 +1,104 @@
 package charts;
 
-import org.jfree.chart.ChartFactory;
-import org.jfree.chart.ChartPanel;
-import org.jfree.chart.ChartUtilities;
-import org.jfree.chart.JFreeChart;
-import org.jfree.chart.plot.PlotOrientation;
-import org.jfree.data.category.DefaultCategoryDataset;
-import org.jfree.ui.ApplicationFrame;
-import org.jfree.ui.RefineryUtilities;
+import competitions.TournamentWithElimination;
+import factory.StrategyReader;
+import javafx.application.Application;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.image.WritableImage;
+import javafx.stage.Stage;
+import org.json.simple.parser.ParseException;
+import player.Player;
+import strategies.genetic.Chromosome;
+import strategies.standard.AlwaysDefectPlayer;
+import strategies.standard.SuspiciousTitForTatPlayer;
+import strategies.standard.TitForTatPlayer;
 
+import javax.imageio.ImageIO;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
-public class TournamentEvolutionLineChart extends ApplicationFrame {
+public class TournamentEvolutionLineChart extends Application {
 
     private static final String CHART_TITLE = "Tournament Evolution";
     private static final String APPLICATION_TITLE = "Line Chart";
-    private static final String CATEGORY_AXIS_LABEL = "Round number";
-    private static final String VALUE_AXIS_LABEL = "Number of players";
-    private static final int WIDTH = 550;
-    private static final int HEIGHT = 350;
+    private static final String X_AXIS_LABEL = "Round number";
+    private static final String Y_AXIS_LABEL = "Number of players";
+    private static final int WIDTH = 800;
+    private static final int HEIGHT = 300;
 
-    public TournamentEvolutionLineChart(DefaultCategoryDataset dataset) throws IOException {
-
-        super(APPLICATION_TITLE);
-
-        JFreeChart lineChart = ChartFactory.createLineChart(
-                CHART_TITLE,
-                CATEGORY_AXIS_LABEL,
-                VALUE_AXIS_LABEL,
-                dataset,
-                PlotOrientation.VERTICAL,
-                true,
-                true,
-                false);
-
-        ChartPanel chartPanel = new ChartPanel(lineChart);
-        chartPanel.setPreferredSize(new java.awt.Dimension(WIDTH, HEIGHT));
-        setContentPane(chartPanel);
-        this.pack();
-        RefineryUtilities.centerFrameOnScreen(this);
-        this.setVisible(true);
-        File file = createNewJpegFile();
-        ChartUtilities.saveChartAsJPEG(file, lineChart, WIDTH + 10, HEIGHT + 10);
+    public static void main(String[] args) {
+        launch(args);
     }
 
-    private static File createNewJpegFile() throws IOException {
-        String path = "src/resources/line_charts/chart_" + System.currentTimeMillis() + ".jpeg";
+    @SuppressWarnings("unchecked")
+    @Override
+    public void start(Stage stage) throws IOException, ParseException {
+
+        stage.setTitle(APPLICATION_TITLE);
+
+        final NumberAxis xAxis = new NumberAxis();
+        final NumberAxis yAxis = new NumberAxis();
+        xAxis.setLabel(X_AXIS_LABEL);
+        yAxis.setLabel(Y_AXIS_LABEL);
+
+        final LineChart<Number, Number> lineChart = new LineChart<>(xAxis, yAxis);
+
+        lineChart.setTitle(CHART_TITLE);
+
+
+        List<Player> players = new ArrayList<>();
+        players.add(new TitForTatPlayer());
+        players.add(new TitForTatPlayer());
+        players.add(new TitForTatPlayer());
+        players.add(new TitForTatPlayer());
+        players.add(new TitForTatPlayer());
+        players.add(new AlwaysDefectPlayer());
+        players.add(new AlwaysDefectPlayer());
+        players.add(new SuspiciousTitForTatPlayer());
+        Chromosome fromFile = StrategyReader.getChromosomeWithStrategyFromJsonFile("src/resources/chromosome_strategies/strategy_1523972359871.json");
+        fromFile.resetScore();
+        players.add(fromFile);
+        players.add(fromFile);
+
+        TournamentWithElimination tournament = new TournamentWithElimination(players, 25, 15);
+        Map<String, XYChart.Series> seriesPerPlayer = tournament.playTournamentAndGenerateSets();
+
+        Scene scene = new Scene(lineChart, WIDTH, HEIGHT);
+
+        for (String playerType:seriesPerPlayer.keySet()) {
+            lineChart.getData().add(seriesPerPlayer.get(playerType));
+        }
+
+        stage.setScene(scene);
+        saveSceneAsPng(scene);
+        stage.show();
+
+    }
+
+    public void saveSceneAsPng(Scene scene) throws IOException {
+        WritableImage image = scene.snapshot(null);
+        File file = createNewPngFile();
+        try {
+            ImageIO.write(SwingFXUtils.fromFXImage(image, null), "png", file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static File createNewPngFile() throws IOException {
+        String path = "src/resources/line_charts/chart_" + System.currentTimeMillis() + ".png";
         File newFile = new File(path);
         newFile.createNewFile();
         return newFile;
     }
+
+
 
 }
--- TheEvolutionOfCooperation/src/competitions/TournamentWithElimination.java
+++ TheEvolutionOfCooperation/src/competitions/TournamentWithElimination.java
@@ -1,19 +1,10 @@
 package competitions;
 
-import charts.TournamentEvolutionLineChart;
-import factory.StrategyReader;
-import org.jfree.data.category.DefaultCategoryDataset;
-import org.json.simple.parser.ParseException;
+import javafx.scene.chart.XYChart;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import player.Player;
-import strategies.genetic.Chromosome;
-import strategies.standard.AlwaysDefectPlayer;
-import strategies.standard.SuspiciousTitForTatPlayer;
-import strategies.standard.TitForTatPlayer;
 
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -24,7 +15,7 @@ public class TournamentWithElimination extends Tournament {
     private final int MAX_ROUNDS = 100;
     private final Logger logger = LoggerFactory.getLogger(TournamentWithElimination.class);
 
-    private TournamentWithElimination(List<Player> players, int percentOfPlayersToEliminate, int numberOfRoundsPerMatch) {
+    public TournamentWithElimination(List<Player> players, int percentOfPlayersToEliminate, int numberOfRoundsPerMatch) {
         super(players, numberOfRoundsPerMatch);
         assert percentOfPlayersToEliminate > 0 && percentOfPlayersToEliminate < 100;
         assert numberOfRoundsPerMatch > 0;
@@ -55,29 +46,41 @@ public class TournamentWithElimination extends Tournament {
         }
     }
 
-    public DefaultCategoryDataset playTournamentAndPopulateDataset() {
+    @SuppressWarnings("unchecked")
+    public Map<String, XYChart.Series> playTournamentAndGenerateSets() {
+
         resetScoreAndNumberOfMatchesCounter();
         int currentRoundNumber = 0;
-        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+
+        Map<String, XYChart.Series> seriesPerPlayer = new HashMap<>();
+        for (Player player : players) {
+            String currentPlayerType = player.getPlayerType();
+            if (!seriesPerPlayer.containsKey(currentPlayerType)) {
+                XYChart.Series currentPlayerTypeSeries = new XYChart.Series();
+                currentPlayerTypeSeries.setName(currentPlayerType);
+                seriesPerPlayer.put(currentPlayerType, currentPlayerTypeSeries);
+            }
+        }
 
         while (!areAllPlayersOfTheSameType() && currentRoundNumber < MAX_ROUNDS) {
             logger.info("Round number " + currentRoundNumber +".");
             Map<String, Integer> typeCounter = getNumberOfPlayersPerType();
             for (String playerType : typeCounter.keySet()) {
-                dataset.addValue((Number) typeCounter.get(playerType), playerType, currentRoundNumber);
+                seriesPerPlayer.get(playerType).getData().add(new XYChart.Data(currentRoundNumber, typeCounter.get(playerType)));
                 logger.info("There are " + typeCounter.get(playerType) + " players of type \"" + playerType + "\".");
             }
             playAllPlayersCombinations();
             reshapePopulation();
             currentRoundNumber++;
         }
+
         logger.info("Round number " + currentRoundNumber +".");
         Map<String, Integer> typeCounter = getNumberOfPlayersPerType();
         for (String playerType : typeCounter.keySet()) {
-            dataset.addValue((Number) typeCounter.get(playerType), playerType, currentRoundNumber);
+            seriesPerPlayer.get(playerType).getData().add(new XYChart.Data(currentRoundNumber, typeCounter.get(playerType)));
             logger.info("There are " + typeCounter.get(playerType) + " players of type \"" + playerType + "\".");
         }
-        return dataset;
+        return seriesPerPlayer;
     }
 
     /**
@@ -122,28 +125,6 @@ public class TournamentWithElimination extends Tournament {
             }
         }
         return true;
-    }
-
-    public static void main(String[] args) throws IOException, ParseException {
-
-        List<Player> players = new ArrayList<>();
-        players.add(new TitForTatPlayer());
-        players.add(new TitForTatPlayer());
-        players.add(new TitForTatPlayer());
-        players.add(new TitForTatPlayer());
-        players.add(new TitForTatPlayer());
-        players.add(new AlwaysDefectPlayer());
-        players.add(new AlwaysDefectPlayer());
-        players.add(new SuspiciousTitForTatPlayer());
-        Chromosome fromFile = StrategyReader.getChromosomeWithStrategyFromJsonFile("src/resources/chromosome_strategies/strategy_1523972359871.json");
-        fromFile.resetScore();
-        players.add(fromFile);
-        players.add(fromFile);
-
-        TournamentWithElimination tournament = new TournamentWithElimination(players, 25, 15);
-        DefaultCategoryDataset dataset = tournament.playTournamentAndPopulateDataset();
-        TournamentEvolutionLineChart chart = new TournamentEvolutionLineChart(dataset);
-
     }
 
 }
